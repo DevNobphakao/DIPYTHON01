d44c15da35bb0bc5b98629625fb64b222ca79e49
DevNobphakao <dev.nobphakao@gmail.com>
2025-09-05 14:53:57 +0700

    V.8

--- workshop30.py
+++ workshop30.py
@@ -1,15 +1,18 @@
+from random import randint
+
 print('..................')
 print('เกมส์ทายตัวเลขที่มีค่าอยู่ที่ 1 - 100 กันเถอะ')
 print('..................')
-while True:
-    guess = int(input(' กรุณาทายตัวเลข: '))
-    if guess == 59 :
-        print(' ยินดีด้วยคุณทายถูก ')
-        break
-    elif guess > 59 :
-        print(' คุณทายผิดตัวเลขที่ป้อนมากไป ')
-    else :
-        print(' คุณทายผิดตัวเลขที่ป้อนน้อยไป ')
+num_rand = randint(1, 100)
+num_user = 0
+while num_user != num_rand:
+    num_user = int(input('ป้อนตัวเลขที่ท่านคิดว่าใช่ : '))
+    if num_user < num_rand:
+        print('ตัวเลขที่ท่านป้อนน้อยเกินไป')
+    elif num_user > num_rand:
+        print('ตัวเลขที่ท่านป้อนมากเกินไป')
+    else:
+        print('เยี่ยมมาก ทายถูกต้องแล้ว')
 
 print('..................')
 print('ขอบคุณที่เล่นเกมส์กับเรา')
